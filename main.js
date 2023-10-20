4cabead719228d22db34ee01ea8ab477f9be64fa
Sâmela <104445773+sasahvicentee@users.noreply.github.com>
2023-10-20 11:30:24 -0300

    a

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-const listaDeTeclas = document.querySelectorAll('tecla');
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-listaDeTeclas[0].onclick = tocaSomPom;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
